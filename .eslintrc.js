93530056ef487a1056d8e95d96d3f0c609523a4d
Adon Irani <a@atriskmedia.com>
2022-02-22 23:51:30 -0500

    I love Gatsby

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+    "env": {
+        "node": true
+    },
+    "extends": [
+        "eslint:recommended"
+    ],
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "rules": {
+    },
+}
