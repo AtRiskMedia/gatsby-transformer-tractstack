467df882de8539d8a92e695cab4b0b62312e4455
Adon Irani <a@atriskmedia.com>
2022-03-12 20:42:33 -0500

    begun onCreateNode handler

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,18 +1,54 @@
 "use strict";
 
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.com/docs/node-apis/
- */
-exports.onCreateNode = function (_ref) {
-  var node = _ref.node,
-      actions = _ref.actions;
-  var DrupalNodes = ['file__file', 'node__tractstack', 'node__story_fragment', 'node__pane'];
+function _typeof(obj) { "@babel/helpers - typeof"; return _typeof = "function" == typeof Symbol && "symbol" == typeof Symbol.iterator ? function (obj) { return typeof obj; } : function (obj) { return obj && "function" == typeof Symbol && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }, _typeof(obj); }
 
-  if (node.internal.owner === "gatsby-source-drupal" && DrupalNodes.includes(node.internal.type)) {
-    console.log("parent node added: ", node.internal.type);
-    console.log(node); // need to preprocess tractstack nodes from drupal
-  }
-};
+var React = _interopRequireWildcard(require("react"));
+
+function _getRequireWildcardCache(nodeInterop) { if (typeof WeakMap !== "function") return null; var cacheBabelInterop = new WeakMap(); var cacheNodeInterop = new WeakMap(); return (_getRequireWildcardCache = function _getRequireWildcardCache(nodeInterop) { return nodeInterop ? cacheNodeInterop : cacheBabelInterop; })(nodeInterop); }
+
+function _interopRequireWildcard(obj, nodeInterop) { if (!nodeInterop && obj && obj.__esModule) { return obj; } if (obj === null || _typeof(obj) !== "object" && typeof obj !== "function") { return { "default": obj }; } var cache = _getRequireWildcardCache(nodeInterop); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (key !== "default" && Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj["default"] = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
+
+function asyncGeneratorStep(gen, resolve, reject, _next, _throw, key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { Promise.resolve(value).then(_next, _throw); } }
+
+function _asyncToGenerator(fn) { return function () { var self = this, args = arguments; return new Promise(function (resolve, reject) { var gen = fn.apply(self, args); function _next(value) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "next", value); } function _throw(err) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "throw", err); } _next(undefined); }); }; }
+
+exports.onCreateNode = /*#__PURE__*/function () {
+  var _ref2 = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee(_ref) {
+    var createNodeId, node, actions, createNode, createNodeField, DrupalNodes;
+    return regeneratorRuntime.wrap(function _callee$(_context) {
+      while (1) {
+        switch (_context.prev = _context.next) {
+          case 0:
+            createNodeId = _ref.createNodeId, node = _ref.node, actions = _ref.actions;
+            createNode = actions.createNode, createNodeField = actions.createNodeField;
+            DrupalNodes = ['paragraph__markdown', 'paragraph__background', 'paragraph__video', 'paragraph__d3', 'paragraph__h5p'];
+
+            if (!(node.internal.owner !== "gatsby-source-drupal" || !DrupalNodes.includes(node.internal.type))) {
+              _context.next = 5;
+              break;
+            }
+
+            return _context.abrupt("return");
+
+          case 5:
+            console.log("paragraph found: ", node.internal.type);
+            console.log(node);
+            createNodeField({
+              node: node,
+              name: "rendered_component",
+              value: /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("p", null, "test"))
+            }); // need to preprocess tractstack nodes from drupal
+
+          case 8:
+          case "end":
+            return _context.stop();
+        }
+      }
+    }, _callee);
+  }));
+
+  return function (_x) {
+    return _ref2.apply(this, arguments);
+  };
+}();
 //# sourceMappingURL=gatsby-node.js.map
--- src/gatsby-node.js
+++ src/gatsby-node.js
@@ -1,14 +1,21 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.com/docs/node-apis/
- */
+import * as React from "react"
 
-exports.onCreateNode = ({ node, actions }) => {
-  const DrupalNodes = ['file__file','node__tractstack', 'node__story_fragment', 'node__pane'];
-  if( node.internal.owner === "gatsby-source-drupal" && DrupalNodes.includes(node.internal.type) ) {
-    console.log( "parent node added: ", node.internal.type );
-    console.log( node );
-    // need to preprocess tractstack nodes from drupal
+exports.onCreateNode = async ({
+  createNodeId,
+  node,
+  actions
+}) => {
+  const { createNode, createNodeField } = actions
+  const DrupalNodes = ['paragraph__markdown','paragraph__background','paragraph__video','paragraph__d3','paragraph__h5p'];
+
+  if( node.internal.owner !== "gatsby-source-drupal" || !DrupalNodes.includes(node.internal.type) ) {
+    return;
   }
+  console.log( "paragraph found: ", node.internal.type );
+  console.log( node );
+  createNodeField({
+    node, name: `rendered_component`,
+    value: <><p>test</p></>
+  });
+  // need to preprocess tractstack nodes from drupal
 }
